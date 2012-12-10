cf72b04dddca98ed5c8070e0beddced7ac401433
Joel Richard <github@joelrichard.com>
2012-12-10 17:08:20 -0500

    Fixed the version numbers in the version.php file so we can use them when building a release.

--- system/application/config/version.php
+++ system/application/config/version.php
@@ -6,5 +6,5 @@
 | This file contains the version and release date of Macaw
 |
 */
-$version_rev = '2.0.431';
-$version_date = 'December 07, 2012';
+$version_rev = 'VERSION_GOES_HERE';
+$version_date = 'DATE_GOES_HERE';
