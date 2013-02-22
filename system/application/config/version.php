a7c7663d324c0659ddddde94c5ed8bb51c0777e3
Joel Richard <github@joelrichard.com>
2013-02-22 08:20:49 -0500

    Updating version to 2.0.4

--- system/application/config/version.php
+++ system/application/config/version.php
@@ -6,5 +6,5 @@
 | This file contains the version and release date of Macaw
 |
 */
-$version_rev = '2.0.3';
-$version_date = 'December 12, 2012';
+$version_rev = '2.0.4';
+$version_date = 'February 22, 2013';
