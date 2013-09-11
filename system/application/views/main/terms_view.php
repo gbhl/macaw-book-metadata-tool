8fe22482e5ce6893856631e0a56c45e9b112ce4f
Joel Richard <github@joelrichard.com>
2013-09-11 13:11:27 -0400

    Updates for the terms and conditions and further adjustments for mysql

---
+++ system/application/views/main/terms_view.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Macaw</title>
+	<? $this->load->view('global/head_view') ?>
+</head>
+<body class="yui-skin-sam">
+<?php
+	$cfg = $this->config->item('macaw');
+?>
+
+<?php if ($this->uri->total_segments() == 0 || $this->uri->segment(1) == 'login') { ?>
+	<div id="doc3">
+		<div id="bd">
+<?php } else { ?>
+	<div id="doc3">
+		<div id="banner">
+			<div id="hd">
+				<div id="title" style="float: left">
+					<img src="<?php echo $this->config->item('base_url'); ?>images/logo.png" alt="logo.png" width="22" height="22" border="0" align="left" id="logo">
+					<div style="float:left">
+					<h4 style="padding-right:10px;">Macaw Metadata Collection and Workflow System <?php if ($cfg['testing']) { echo(' <span style="color:#F90;">&nbsp;&nbsp;&nbsp;&nbsp;DEVELOPMENT VERSION</span>'); }?></h4>
+					</div>
+					<div class="clear"><!-- --></div>
+				</div>
+			</div>
+		</div>
+		<div class="messagediv">
+			<?php $this->load->view('global/error_messages_view') ?>
+		</div>	
+		<div id="bd">
+<?php } ?>
+	
+	<!-- removed messaging and put it in the headerview -->
+
+	<div id="main" class="terms">
+		<h1>Terms and Conditions</h1>
+		Please review and agree to the terms of using Macaw before continuing.<br><br>
+		<form id="terms" action="terms_save" method="post">
+			<textarea cols="100" rows="15"><? echo $terms ?></textarea><br><br>
+			<input type="checkbox" name="agree" value="1" id="terms_agree"> <label for="terms_agree">I understand and agree to these terms and conditions</label><br><br>
+			<input type="submit" value="Continue &gt;&gt;">
+		</form>
+	</div>
+	<? $this->load->view('global/footer_view') ?>
+</body>
+</html>
