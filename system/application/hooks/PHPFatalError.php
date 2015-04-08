b816d10e30205240675d688d0207c46ae3e669a4
Joel Richard <github@joelrichard.com>
2015-04-08 09:14:54 -0400

    Changed the PHP Fatal Error handler to not create more errors while on the command line. Maybe. This is a strange file.

--- system/application/hooks/PHPFatalError.php
+++ system/application/hooks/PHPFatalError.php
@@ -9,24 +9,31 @@ class PHPFatalError {
 function handleShutdown() {
 	if (($error = error_get_last())) {
 		$buffer = ob_get_contents();
-		ob_clean();
-		# raport the event, send email etc.
-		
-		$msg= $buffer;
-		$um = 'We have found some error please try again later.';
-		ob_start();
-		$data = array();
-		$CI = get_instance();
-		if (!$CI->config->item('DEBUG_PRINT')) {
-			$msg='';
+		if ($buffer) {
+			ob_clean();
 		}
-		$data['print_msg'] = $msg;
-		$data['um'] = $um;
+		echo $buffer."\n";
+		echo 'ERROR: '.$error['message']."\n";
+		echo 'FILE: '.$error['file']."\n";
+		echo 'LINE: '.$error['line']."\n";
 		
-		load_page('exception', $data);
-		$buffer = ob_get_contents();
-		ob_end_clean();
-		echo $buffer; 
+// 		# raport the event, send email etc.
+// 		
+// 		$msg = $buffer;
+// 		$um = 'We have found some error please try again later.';
+// 		ob_start();
+// 		$data = array();
+// 		$CI = get_instance();
+// 		if (!$CI->config->item('DEBUG_PRINT')) {
+// 			$msg='';
+// 		}
+// 		$data['print_msg'] = $msg;
+// 		$data['um'] = $um;
+// 		
+// 		load_page('exception', $data);
+// 		$buffer = ob_get_contents();
+// 		ob_end_clean();
+// 		echo $buffer; 
 		exit();
 		# from /error-capture, you can use another redirect, to e.g. home page
 	}
