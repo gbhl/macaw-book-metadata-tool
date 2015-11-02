8a827590d014dd15980b7a5602b215ece6436e91
Joel Richard <github@joelrichard.com>
2015-11-02 14:13:14 -0500

    Updated CSV import to be more restrictive of encoding detection.

--- system/application/controllers/utils.php
+++ system/application/controllers/utils.php
@@ -363,7 +363,37 @@ class Utils extends Controller {
 				$all_file = preg_replace('/,Mar\-52,/', ',marc6552,', $all_file);
 				file_put_contents($fname, $all_file);
 			}
-			$encoding = mb_detect_encoding($all_file, mb_list_encodings(), true);
+			$allowed_encodings = array(
+				'ASCII',
+				'7bit',
+				'8bit',
+				'UTF-8',
+				'UTF-16',
+				'UTF-16BE',
+				'UTF-16LE',
+				'UTF-32',
+				'UTF-32BE',
+				'UTF-32LE',
+				'ISO-8859-1',
+				'ISO-8859-2',
+				'ISO-8859-3',
+				'ISO-8859-4',
+				'ISO-8859-5',
+				'ISO-8859-6',
+				'ISO-8859-7',
+				'ISO-8859-8',
+				'ISO-8859-9',
+				'ISO-8859-10',
+				'ISO-8859-13',
+				'ISO-8859-14',
+				'ISO-8859-15',
+				'ISO-8859-16',
+				'Windows-1252',
+				'Windows-1254',
+				'Windows-1251',
+			);
+			
+			$encoding = mb_detect_encoding($all_file, $allowed_encodings, true);
 			unset($all_file);
 			
 			$row = 1;
