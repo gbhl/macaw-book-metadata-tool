aba7dd34d4b796ec8a9d3edae4945e6ec3eacea3
Joel Richard <github@joelrichard.com>
2018-08-07 09:35:02 -0400

    Updated version number

--- system/application/config/version.php
+++ system/application/config/version.php
@@ -6,5 +6,5 @@
 | This file contains the version and release date of Macaw
 |
 */
-$version_rev = '2.5.2';
-$version_date = 'March 1, 2018';
+$version_rev = '2.5.4';
+$version_date = 'July 10, 2018';
