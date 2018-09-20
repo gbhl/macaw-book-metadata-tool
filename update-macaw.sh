14fae8890dd4e8df33e65551f0c1a83f0beaf5b6
Joel Richard <github@joelrichard.com>
2018-09-20 12:26:13 -0400

    Updated update script for php7

--- update-macaw.sh
+++ update-macaw.sh
@@ -27,12 +27,12 @@ fi
 
 echo "Changing to temporary directory..."
 pushd /tmp
-rm -fr macaw-book-metadata-tool-master
+rm -fr macaw-book-metadata-tool-*
 echo "Getting latest code from GitHub..."
 curl -O -L https://github.com/gbhl/macaw-book-metadata-tool/archive/php7.zip
 echo "Unzipping code from github..."
-unzip -q master.zip
-cd macaw-book-metadata-tool-master
+unzip -q php7.zip
+cd macaw-book-metadata-tool-php7
 echo "Copying new code into Macaw installation at $MACAW_PATH..."
 sudo cp -a * $MACAW_PATH/. 
 echo "Changing ownership of Macaw files to $WWW_USER"
