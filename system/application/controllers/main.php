296bea11733562283630ef9814a4dfb405d6348c
Joel Richard <github@joelrichard.com>
2018-11-07 14:20:28 -0500

    Added handlers for the dropdown in the In Progress list.

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -987,7 +987,7 @@ class Main extends Controller {
 	 * @since Version 1.0
 	 */
 	function listitems() {
-		$data['filter'] = array('all', 'new', 'in progress', 'completed');
+		$data['filter'] = array('All', 'New', 'In Progress', 'In QA', 'Completed');
 		$this->load->view('main/queue_view', $data);
 	}
 
