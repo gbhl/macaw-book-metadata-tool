57b081f7d8e4a599864001c479cd6a3d6c2d4468
Joel Richard <github@joelrichard.com>
2020-12-16 10:59:38 -0500

    Removed v1 sql files.

--- system/application/sql/macaw-pgsql-1.3.sql
+++
@@ -1,8 +0,0 @@
--- Required Version: 1.3.105
-ALTER TABLE item ADD missing_pages boolean;
-ALTER TABLE item ADD pages_found integer;
-ALTER TABLE item ADD pages_scanned integer;
-ALTER TABLE item ADD scan_time integer;
-
-ALTER TABLE page ADD is_missing boolean;
-
