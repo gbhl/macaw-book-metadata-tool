7a69dd5d58dd66bfce22a9175ab65250aa08841c
Joel Richard <cajunjoel@gmail.com>
2022-09-13 08:37:07 -0400

    Switched dashboard to chart.js, not google.

--- system/application/views/dashboard/dashboard_view.php
+++ system/application/views/dashboard/dashboard_view.php
@@ -6,31 +6,27 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
 	<title>Dashboard | Macaw</title>
 	<?php $this->load->view('global/head_view') ?>
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-	<script type="text/javascript">
-		google.load("visualization", "1", {packages:["corechart"]});
-		google.setOnLoadCallback(YAHOO.macaw.Dashboard.init);
-	</script>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
+	<style>
+		.canvasjs-chart-credit {color:white !important}
+	</style>
 </head>
 <body class="yui-skin-sam">
 	<?php $this->load->view('global/header_view') ?>
 	<h1>Statistics</h1>
 	<table id="dashboard" style="margin: 0 auto;">
 		<tr>
-			<td width="50%" class="widget"><h3>Summary</h3><div id="summary"></div></td>
-			<td width="50%" class="widget"><h3>Disk Usage (%)</h3><div id="disk"></div></td>
+			<td width="50%" class="widget"><h3>Summary</h3><div id="summary" width="350" height="220"></div></td>
+			<td width="50%" class="widget"><h3>Disk Usage (%)</h3><canvas id="disk" width="350" height="220"></canvas></td>
 		</tr>
 		<tr>
-			<td class="widget"><h3>Pages Scanned Per Day</h3><div id="perday"></div></td>
-			<td class="widget"><h3>Total Pages Scanned</h3><div id="pages"></div></td>
+			<td class="widget"><h3>Pages Scanned Per Day</h3><canvas id="perday" width="350" height="220"></canvas></td>
+			<td class="widget"><h3>Total Pages Scanned</h3><canvas id="pages" width="350" height="220"></canvas></td>
 		</tr>	
 	</table>
 
 	<?php $this->load->view('global/footer_view') ?>
-	<script type="text/javascript">
-		YAHOO.util.Dom.get("txtBarcode").focus();
-	</script>
-
+	<script type="text/javascript">YAHOO.util.Event.onDOMReady(YAHOO.macaw.Dashboard.init);</script>
 </body>
 </html>
 
