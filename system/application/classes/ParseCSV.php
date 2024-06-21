4fa677d7e8a36511fed7ddea212f017c5214cd35
Joel Richard <github@joelrichard.com>
2024-06-21 08:28:15 -0400

    Fix deprecated arrays

--- system/application/classes/ParseCSV.php
+++ system/application/classes/ParseCSV.php
@@ -573,9 +573,9 @@ class parseCSV {
 
         // walk specific depth finding posssible delimiter characters
         for ($i=0; $i < $strlen; $i++) {
-            $ch  = $data{$i};
-            $nch = (isset($data{$i+1})) ? $data{$i+1} : false ;
-            $pch = (isset($data{$i-1})) ? $data{$i-1} : false ;
+            $ch  = $data[$i];
+            $nch = (isset($data[$i+1])) ? $data[$i+1] : false ;
+            $pch = (isset($data[$i-1])) ? $data[$i-1] : false ;
 
             // open and closing quotes
             if ($ch == $enclosure) {
@@ -691,16 +691,16 @@ class parseCSV {
 
         // force the parser to process end of data as a character (false) when
         // data does not end with a line feed or carriage return character.
-        $lch = $data{$strlen-1};
+        $lch = $data[$strlen-1];
         if ($lch != "\n" && $lch != "\r") {
             $strlen++;
         }
 
         // walk through each character
         for ($i=0; $i < $strlen; $i++) {
-            $ch  = (isset($data{$i}))   ? $data{$i}   : false;
-            $nch = (isset($data{$i+1})) ? $data{$i+1} : false;
-            $pch = (isset($data{$i-1})) ? $data{$i-1} : false;
+            $ch  = (isset($data[$i]))   ? $data[$i]   : false;
+            $nch = (isset($data[$i+1])) ? $data[$i+1] : false;
+            $pch = (isset($data[$i-1])) ? $data[$i-1] : false;
 
             // open/close quotes, and inline quotes
             if ($ch == $this->enclosure) {
@@ -731,8 +731,8 @@ class parseCSV {
                     $i++;
                 }
                 elseif ($nch != $this->delimiter && $nch != "\r" && $nch != "\n") {
-                    for ($x=($i+1); isset($data{$x}) && ltrim($data{$x}, $white_spaces) == ''; $x++) {}
-                    if ($data{$x} == $this->delimiter) {
+                    for ($x=($i+1); isset($data[$x]) && ltrim($data[$x], $white_spaces) == ''; $x++) {}
+                    if ($data[$x] == $this->delimiter) {
                         $enclosed = false;
                         $i        = $x;
                     }
@@ -1109,7 +1109,7 @@ class parseCSV {
         if ($value !== null && $value != '') {
             $delimiter_quoted = preg_quote($delimiter, '/');
             $enclosure_quoted = preg_quote($this->enclosure, '/');
-            if (preg_match("/".$delimiter_quoted."|".$enclosure_quoted."|\n|\r/i", $value) || ($value{0} == ' ' || substr($value, -1) == ' ') || $this->enclose_all) {
+            if (preg_match("/".$delimiter_quoted."|".$enclosure_quoted."|\n|\r/i", $value) || ($value[0] == ' ' || substr($value, -1) == ' ') || $this->enclose_all) {
                 $value = str_replace($this->enclosure, $this->enclosure.$this->enclosure, $value);
                 $value = $this->enclosure.$value.$this->enclosure;
             }
