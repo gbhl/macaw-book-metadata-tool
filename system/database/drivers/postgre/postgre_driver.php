57adca9a57a1475f4fdcdfb57df6c424472f4e99
Joel Richard <github@joelrichard.com>
2025-03-05 08:29:39 -0500

    Force pgsql version to 8.1+

--- system/database/drivers/postgre/postgre_driver.php
+++ system/database/drivers/postgre/postgre_driver.php
@@ -283,8 +283,6 @@ class CI_DB_postgre_driver extends CI_DB {
 	 */
 	function insert_id()
 	{
-		// $v = $this->_version();
-		// $v = $v['server'];
 		$v = '8.1';
 		
 		$table	= func_num_args() > 0 ? func_get_arg(0) : null;
