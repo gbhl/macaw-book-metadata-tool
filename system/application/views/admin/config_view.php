683b1df81df12e4ec9e0f7206235ca570c052322
Joel Richard <github@joelrichard.com>
2025-10-14 08:33:13 -0400

    Added option for phpinfo() output

--- system/application/views/admin/config_view.php
+++ system/application/views/admin/config_view.php
@@ -8,6 +8,30 @@
 		<!-- 29/05/12 temp insert to retain old style -->
 	<link rel="stylesheet" type="text/css" href="/css/yui-combo.css"> 
 	<?php $this->load->view('global/head_view') ?>
+  <?php if ($all) { ?>
+    <style type="text/css">
+
+      #phpinfo {}
+      #phpinfo pre {}
+      #phpinfo a:link {}
+      #phpinfo a:hover {}
+      #phpinfo table {}
+      #phpinfo .center {}
+      #phpinfo .center table {}
+      #phpinfo .center th {}
+      #phpinfo td, th {}
+      #phpinfo h1 {}
+      #phpinfo h2 {}
+      #phpinfo .p {}
+      #phpinfo .e {}
+      #phpinfo .h {}
+      #phpinfo .v {}
+      #phpinfo .vr {}
+      #phpinfo img {}
+      #phpinfo hr {}      
+    </style>
+
+  <?php } ?>
 </head>
 <body class="yui-skin-sam" id="logsbody">
 	<?php $this->load->view('global/header_view') ?>
@@ -19,16 +43,36 @@
         <tr><th>Paramter</th><th>Value</th></tr>
       </thead>
       <tbody>
-        <?php foreach ($config_params as $s) {
+        <?php 
+        foreach ($config_params as $s) {
           echo "<tr>";
             echo "<td>".$s['paramater']."</td>";
             echo "<td>".$s['value']."</td>";
           echo "</tr>";
         }
+        if ($all) {
+          echo "<tr>";
+          echo "<td><strong>PHP Configuration</strong></td>";
+          echo "<td>##############################################################</td>";
+        echo "</tr>";
+      foreach (array_keys($phpinfo) as $section) {
+            echo "<tr>";
+              echo "<td><strong>".$section."</strong></td>";
+              echo "<td>##############################################################</td>";
+            echo "</tr>";
+            foreach ($phpinfo[$section] as $k => $v) {
+              echo "<tr>";
+                echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;".$k."</td>";
+                echo "<td>".print_r($v,true)."</td>";
+              echo "</tr>";
+            }
+          }
+
+        }
         ?>
       </tbody>
     </table>
-  </div>   
+  </div>
 	<script type="text/javascript">
 		YAHOO.util.Event.onDOMReady(SimpleTable.init);
 	</script>
